20a118d9e11eee3eb02aa2f0d467457c50d6a5b8
Laszlo Deak <ladeak@windowslive.com>
2020-05-31 11:02:08 +0200

    NXunit Initial projects

---
+++ src/ConvertNxUnitAnalyzer/ConvertNxUnitAnalyzer.Test/ConvertNxUnitAnalyzerUnitTests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ConvertNxUnitAnalyzer;
+using Verify = Microsoft.CodeAnalysis.CSharp.Testing.MSTest.CodeFixVerifier<
+    ConvertNxUnitAnalyzer.ConvertNxUnitAnalyzer,
+    ConvertNxUnitAnalyzer.ConvertNxUnitCodeFixProvider>;
+using Microsoft.CodeAnalysis.CSharp.Testing;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.CodeAnalysis.Testing.Verifiers;
+using System.Collections.Immutable;
+
+namespace ConvertNxUnitAnalyzer.Test
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public async Task EmptyDocument_NoDiagnostics()
+        {
+            var test = @"";
+
+            await Verify.VerifyAnalyzerAsync(test);
+        }
+
+        [TestMethod]
+        public async Task NUnitTestAttribute_DiagnosticWarning()
+        {
+            var source = @"
+using NUnit.Framework;
+
+namespace NUnitToXUnitTests
+{
+    public class NUnitTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}";
+
+            var expected = Verify.Diagnostic("ADConvertNxUnitAnalyzer").WithLocation(8, 9).WithArguments("Test1");
+
+            var fixture = new CSharpCodeFixTest<ConvertNxUnitAnalyzer, ConvertNxUnitCodeFixProvider, MSTestVerifier>();
+            fixture.ReferenceAssemblies = fixture.ReferenceAssemblies.AddPackages(ImmutableArray.Create(new PackageIdentity("nunit", "3.12.0")));
+            fixture.TestCode = source;
+            fixture.ExpectedDiagnostics.Add(expected);
+            await fixture.RunAsync();
+        }
+
+
+        [TestMethod]
+        public async Task TestMethod2()
+        {
+            var test = @"
+using NUnit.Framework;
+
+namespace NUnitToXUnitTests
+{
+    public class NUnitTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}";
+
+            var fixtest = @"using Xunit;
+
+namespace NUnitToXUnitTests
+{
+    public class xUnitTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}";
+
+            var expected = Verify.Diagnostic("ADConvertNxUnitAnalyzer").WithLocation(11, 15).WithArguments("TypeName");
+            await Verify.VerifyCodeFixAsync(test, expected, fixtest);
+        }
+    }
+}
---
+++ src/ConvertNxUnitAnalyzer/ConvertNxUnitAnalyzer/ConvertNxUnitAnalyzer.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace ConvertNxUnitAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ConvertNxUnitAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "ADConvertNxUnitAnalyzer";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Unit Tests";
+
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+            context.RegisterSyntaxNodeAction(AnalyzeSyntax, SyntaxKind.MethodDeclaration);
+        }
+
+        private void AnalyzeSyntax(SyntaxNodeAnalysisContext context)
+        {
+            if (context.Node is MethodDeclarationSyntax methodSyntax && methodSyntax.AttributeLists.Any())
+            {
+                foreach (var attribute in methodSyntax.AttributeLists.SelectMany(x => x.Attributes))
+                {
+                    var attributeSymbol = context.SemanticModel.GetSymbolInfo(attribute).Symbol as IMethodSymbol;
+                    if (IsSymbol(attributeSymbol, ".ctor", "TestAttribute"))
+                    {
+                        var diagnostic = Diagnostic.Create(Rule, methodSyntax.GetLocation(), methodSyntax.Identifier.ValueText);
+                        context.ReportDiagnostic(diagnostic);
+                    }
+                }
+            }
+        }
+
+        private static bool IsSymbol(ISymbol symbolsType, string name, string type, string assembly = "nunit.framework")
+        {
+            return symbolsType.Name == name
+                && symbolsType.OriginalDefinition.ContainingAssembly.MetadataName == assembly
+                && symbolsType.OriginalDefinition.ContainingType.Name == type;
+        }
+    }
+}
