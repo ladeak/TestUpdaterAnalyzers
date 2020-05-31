481359a3d659525fe094c550e9f0072e474df1e1
Laszlo Deak <ladeak@windowslive.com>
2020-05-31 11:02:08 +0200

    NXunit Initial projects

---
+++ sample/NUnitToXUnitTests/NUnitTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace NUnitToXUnitTests
+{
+    public class NUnitTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ sample/NUnitToXUnitTests/xUnitTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace NUnitToXUnitTests
+{
+    public class xUnitTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
