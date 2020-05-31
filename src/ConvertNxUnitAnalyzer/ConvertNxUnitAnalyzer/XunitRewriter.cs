fa40db0947543979db5190c703cc0ff5055f0aba
Laszlo Deak <ladeak@windowslive.com>
2020-05-31 15:51:21 +0200

    Replacing NUnit usings and Test attribute

---
+++ src/ConvertNxUnitAnalyzer/ConvertNxUnitAnalyzer/XunitRewriter.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+
+namespace ConvertNxUnitAnalyzer
+{
+    public class XunitRewriter : CSharpSyntaxRewriter
+    {
+        private SemanticModel _semanticModel;
+        private Document _originalDocument;
+
+        public async Task<Document> UpdateToXUnitAsync(Document document, SemanticModel semanticModel, TextSpan diagnosticSpan, CancellationToken cancellationToken)
+        {
+            _semanticModel = semanticModel;
+            _originalDocument = document;
+            var root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+
+            var newRoot = Visit(root);
+            return document.WithSyntaxRoot(newRoot);
+        }
+
+        public override SyntaxNode VisitAttribute(AttributeSyntax node)
+        {
+            var symbolInfo = _semanticModel.GetSymbolInfo(node).Symbol;
+            var inner = base.VisitAttribute(node);
+
+            if (NUnitRecognizer.IsTestAttribute(symbolInfo))
+            {
+                return SyntaxFactory.Attribute(SyntaxFactory.IdentifierName("Fact"));
+            }
+            return inner;
+        }
+
+        public override SyntaxNode VisitUsingDirective(UsingDirectiveSyntax node)
+        {
+            var symbolInfo = _semanticModel.GetSymbolInfo(node.Name).Symbol;
+            var inner = base.VisitUsingDirective(node);
+
+            if (NUnitRecognizer.IsNUnitUsingDirective(symbolInfo))
+            {
+                return SyntaxFactory.UsingDirective(SyntaxFactory.IdentifierName("Xunit"));
+            }
+            return inner;
+        }
+
+    }
+}
